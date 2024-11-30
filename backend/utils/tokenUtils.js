b3cbf8458847b8b3bdc7322feb16dfaec900d0a2
RavanSahil21 <143107312+sahilravan@users.noreply.github.com>
2024-11-30 12:49:56 +0530

    Create tokenUtils.js

---
+++ backend/utils/tokenUtils.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(decoded);
+      }
+    });
+  });
+};
